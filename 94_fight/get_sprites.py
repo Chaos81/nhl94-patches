4d451dd768ca3ad692001ff48f2e0bb81f0adb5f
Chaos81 <39440267+Chaos81@users.noreply.github.com>
2025-05-18 09:53:38 -0400

    Update get_sprites.py

--- 94_fight/get_sprites.py
+++ 94_fight/get_sprites.py
@@ -13,8 +13,53 @@ class Anim:
                 self.facedir = []       # 8 entries(0-7 directions) of a list of dicts (frame, time keys)
                 self.attrib = 0
 
+def getFrmData(frame, file, sprfile):
+# Get Frame Data and Sprite Data for files given
+# # of sprites in frame = ((Next Frame offset - Current Frame offset) / 8) -1
 
+        frmdata = []
+        count = 0
 
+        with open(file, 'rb') as f:
+                f.seek(int(frame, 16) * 2, 0)   # move to frame offset
+                offset = f.read(2).hex()
+                nextoff = f.read(2).hex()
+                numsprites = ((int(nextoff, 16) - int(offset, 16)) / 8) - 1
+                
+
+                
+
+def getSprData(file):
+# Organize Sprite Data from the file given
+# Each group of Sprite Data is 8 bytes long
+#       Byte 0-1 - X Global Position
+#       Byte 2-3 - Y Global Position
+#       Byte 4-5 - Sprite Tile Offset
+#       Byte 6   - H/V Flip priority and Palette
+#       Byte 7   - Sizetab byte (Tile layout)
+
+        tilelayout = ['1x1','1x2','1x3','1x4','2x1','2x2','2x3','2x4','3x1','3x2','3x3','3x4','4x1','4x2','4x3','4x4']
+        sprdata = []
+        count = 0
+        size = os.path.getsize(file)    # size of file
+        print(size)
+        count = size / 8        # # of sprite data in file
+        print(count)
+        with open(file, 'rb') as f:
+                while count > 1:        # last sprite data is garbage
+                        xoff = f.read(2).hex()
+                        yoff = f.read(2).hex()
+                        toff = f.read(2).hex()
+                        flippal = f.read(1).hex()
+                        sizetab = f.read(1)
+                        layout = tilelayout[int.from_bytes(sizetab, "big")]
+                        sprbytes = dict({'Xoffset': xoff, 'Yoffset': yoff, 'TilePtr': toff, 'HVFlippal': flippal, 
+                                'Sizetab': sizetab.hex(), 'Layout': layout})
+                        print(sprbytes)
+                        sprdata.append(sprbytes)
+                        count -= 1
+                        
+        return sprdata
 
 def getFrames(f, animlist):
 # Retrieve frame list for SPA, one direction at a time
@@ -54,12 +99,15 @@ FrmData_path = os.path.join(script_dir, '93_Tables/FrmSprDataOff.bin')
 Hotlist_path = os.path.join(script_dir, '93_Tables/Hotlist.bin')
 SprData_path = os.path.join(script_dir, '93_Tables/SprData.bin')
 
-
-
-
-
-
 print ("get_sprites.py Version 0.2")
+
+# Collect Sprite data and store it in a list of dicts
+
+sprdata = getSprData(SprData_path)
+
+# Collect frame data and store it in a list of dicts
+frmdata = getFrmData(FrmData_path, SprData_path)
+
 spa = 0
 
 while spa != 'exit':
